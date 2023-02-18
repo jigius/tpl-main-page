33f837433e9f0eff4809a113c59f34f5a1925660
jigius <jigius@gmail.com>
2023-02-18 20:51:39 +0700

    remove an unneeded fonts

--- src/js/fonts.js
+++ src/js/fonts.js
@@ -1,7 +1,7 @@
-import "@fontsource/lexend/300.css";
+/*import "@fontsource/lexend/300.css";
 import "@fontsource/lexend/400.css";
 import "@fontsource/lexend/500.css";
-import "@fontsource/lexend/600.css";
+import "@fontsource/lexend/600.css";*/
 
 import "@fontsource/pt-sans/400.css";
 import "@fontsource/pt-sans/700.css";
